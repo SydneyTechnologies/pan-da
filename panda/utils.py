73d4f6b6c23d8bbd55aa2d9a7a144245fbab55b1
sydneyIdundun <sydney.idundun@gmail.com>
2022-10-15 11:13:49 +0400

    fixed the title regex expression

--- panda/utils.py
+++ panda/utils.py
@@ -3,7 +3,7 @@ import re
 def GetMovieInfo(MovieDetails):
     result = {}
     rspec = r'[0-9]+p.*'
-    rtitle = r'/.*?[0-9]\s/'
+    rtitle = r'/.*?[0-9]\s'
     movie_details = re.search(rspec, MovieDetails).group()
     title = re.search(rtitle, MovieDetails).group()
     return [title, movie_details]
