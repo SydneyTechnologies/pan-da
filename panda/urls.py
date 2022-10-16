90896874a2e4e2656a79def7723ccfb26fc3a3ed
sydneyIdundun <sydney.idundun@gmail.com>
2022-10-16 10:43:19 +0400

    changed the name for the token end point

--- panda/urls.py
+++ panda/urls.py
@@ -3,7 +3,7 @@ from . views import searchMovie, getMovie, LoginView, CreateUserView
 
 urlpatterns = [
     path('signup', view = CreateUserView.as_view(), name ="create"),
-    path('login', view = LoginView.as_view(), name="login"),
+    path('token', view = LoginView.as_view(), name="login"),
     path('search/<str:search>', view = searchMovie, name="search"),
     path('get/<str:link>', view = getMovie, name="get"),
 ]
