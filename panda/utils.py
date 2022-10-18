1bf27a361c98879935f80f549a61c844bd9fbb6a
sydneyIdundun <sydney.idundun@gmail.com>
2022-10-18 10:05:13 +0400

    I was not returning anything in the generate Hash function

--- panda/utils.py
+++ panda/utils.py
@@ -31,5 +31,6 @@ def generateHash():
     numbers = "".join(str(i) for i in list(range(0, 10)))
     final_list = lowercase + uppercase + numbers
     hash = [ random.choice(final_list) for sel in range(7)]
-    print("".join(hash))
+    hash = "".join(hash)
+    return hash
 
